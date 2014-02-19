c82c4624886265b5779ea18ba600c1d1f9a85a29
Fredrik Leijon <fredrik.leijon@gmail.com>
2014-02-19 08:29:49 +0100

    save changes & write to disk

--- designer/app.js
+++ designer/app.js
@@ -11,11 +11,18 @@ lbs.apploader.register('designer', function () {
             libs: ["underscore-min.js"]
         }
     };
+
+    function saveChanges() {
+        lbs.loader.saveLocalFile(lbs.limeDataConnection.ActiveInspector.Class.Name+".html", btoa($("#template").text()));
+    }
+
     //initialize
     self.initialize = function (node,viewModel) {
         
         editor.setup(viewModel.inspector);
         $("#template").text(lbs.loader.loadLocalFileToString(lbs.limeDataConnection.ActiveInspector.Class.Name+".html"));
+        $("#SaveBtn").on("click", saveChanges);
+
         editor.load();
 
         return viewModel;
