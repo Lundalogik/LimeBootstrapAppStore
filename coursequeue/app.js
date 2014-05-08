bdf919964aec6360b130f59ccb0fe653bb1308a2
anttihei <antti.heinila@lundalogik.fi>
2014-05-08 21:47:04 +0300

    release

---
+++ coursequeue/app.js
@@ -0,0 +1,97 @@
+lbs.apploader.register('queue', function () {
+    var self = this;
+    //config
+    self.config = {
+        color:"",
+		flashColor:"",
+        displayText:"",
+		iconPosition:"",
+        icon:"",
+		blinktime:"",
+        dataSources: [
+            {type: 'xml', source: 'Queue.GetQueueLength', alias:"queueLength"}
+        ],
+        resources: {
+            scripts: ['jquery-ui-1.10.4.js'],
+            styles: ['app.css'],
+            libs: []
+        },
+    },
+
+    //initialize
+    self.initialize = function (node,viewmodel) {
+            viewmodel.queueLength =  viewmodel.queueLength.data.queue.value.maxqueue;
+            if(self.config.displayText){
+                viewmodel.displayText = self.config.displayText;
+            }else{
+                viewmodel.displayText = "in Queue";    
+            }
+
+            viewmodel.iconPosition=self.config.iconPosition;
+
+            viewmodel.icon = self.config.icon;
+            viewmodel.color = function(){
+                switch(self.config.color){
+                    case "blue":
+                        return "#d6417d";
+                    break;
+                    case "darkgrey":
+                        return "rgb(176, 176, 176)";
+                    break;
+                    case "red":
+                        return "rgb(232, 89, 89)";
+                    break;
+                    case "pink":
+                        return "rgb(243, 150, 206)";
+                    break;
+                    case "orange":
+                        return "rgb(244, 187, 36)";
+                    break;
+                    case "green":
+                        return "rgb(153, 216, 122)";
+                    break;
+                    default:
+                        return self.config.color;
+                    break;
+                }
+
+            }
+			
+			viewmodel.flashColor = function(){
+                switch(self.config.flashColor){
+                    case "blue":
+                        return "#e85959";
+                    break;
+                    case "darkgrey":
+                        return "rgb(176, 176, 176)";
+                    break;
+                    case "red":
+                        return "#e85959";
+                    break;
+                    case "pink":
+                        return "rgb(243, 150, 206)";
+                    break;
+                    case "orange":
+                        return "rgb(244, 187, 36)";
+                    break;
+                    case "green":
+                        return "rgb(153, 216, 122)";
+                    break;
+                    default:
+                        return self.config.flashColor;
+                    break;
+                }
+
+            }
+
+            viewmodel.addToQueue = function(){
+                lbs.common.executeVba("Queue.AddToQueue");
+				$("#queuebox").effect("highlight", {color: '#41d69a'}, 700);
+				//$("#queuebox" ).effect( "bounce", { times: 3 }, "slow" );
+            }
+            return  viewmodel;
+
+    }
+
+});
+
---
+++ coursequeue/install/Queue.bas
@@ -0,0 +1,62 @@
+Attribute VB_Name = "Queue"
+Public Function GetQueueLength() As String
+On Error GoTo ErrorHandler
+
+    Dim oProcedure As New LDE.Procedure
+    
+    Set oProcedure = Database.Procedures("csp_getQueueLength")
+    
+    oProcedure.Parameters("@@idcampaign").InputValue = ActiveInspector.Controls.GetValue("campaign")
+    
+    oProcedure.log = True
+    
+    'Run the procedure synchronously
+    
+    oProcedure.Execute False
+    
+    GetQueueLength = oProcedure.Result
+    
+    
+Exit Function
+ErrorHandler:
+    UI.ShowError ("Queue.GetQueueLength")
+End Function
+
+Public Sub UpdateQueue()
+On Error GoTo ErrorHandler
+
+    Dim oProcedure As New LDE.Procedure
+    
+    Set oProcedure = Database.Procedures("csp_updateCampaignQueue")
+    
+    oProcedure.Parameters("@@idcampaign").InputValue = ActiveInspector.Controls.GetValue("campaign")
+    
+    oProcedure.log = True
+    
+    'Run the procedure synchronously
+    
+    oProcedure.Execute False
+    
+Exit Sub
+ErrorHandler:
+    UI.ShowError ("Queue.UpdateQueue")
+End Sub
+
+    
+Public Sub addToQueue()
+On Error GoTo ErrorHandler:
+    
+    Dim oInspector As Lime.Inspector
+    
+    Set oInspector = Application.ActiveInspector
+    Call oInspector.Controls.SetValue("queuetime", VBA.Now())
+    
+    Call UpdateQueue
+    
+    Call oInspector.Close(True)
+    
+Exit Sub
+ErrorHandler:
+    UI.ShowError ("Queue.addToQueue")
+End Sub
+
