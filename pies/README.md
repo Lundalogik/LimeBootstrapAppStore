1dab643f9e9b643572d17a714125296108ebc779
andreasastrom <andreas.astr@gmail.com>
2014-06-02 13:25:59 +0200

    Pies
    New app

---
+++ pies/README.md
@@ -0,0 +1,20 @@
+#Pies
+##About
+
+###Installation
+```html
+ <div data-app="{app:'pie',
+	config:{
+			fields: 
+				[
+					{field: 'value1', color: '#660099'},
+					{field: 'value2', color: '#FFCC33'},					
+					{field: 'value3', color: '#3366FF'},					
+					{field: 'value6', color: '#009966'},
+					{field: 'value7', color: '#F7464A'}
+				]
+			,options: {segmentStrokeWidth:2,percentageInnerCutout:80}
+	}
+}">	
+</div>
+```
---
+++ pies/app.js
@@ -0,0 +1,73 @@
+lbs.apploader.register('pie', function () {
+    var self = this;    
+    /*Config (version 2.0)
+        This is the setup of your app. Specify which data and resources that should loaded to set the enviroment of your app.
+        App specific setup for your app to the config section here, i.e self.config.yourPropertiy:'foo'
+        The variabels specified in "config:{}", when you initalize your app are available in in the object "appConfig".
+    */
+
+    self.config =  function(appConfig){            
+            this.yourPropertyDefinedWhenTheAppIsUsed = appConfig.yourProperty;
+            this.fields = appConfig.fields;
+            this.options = appConfig.options;
+            this.dataSources = [{type: 'activeInspector'}];            
+            this.resources = {
+                scripts: ['Chart.min.js'], // <= External libs for your apps. Must be a file
+                styles: ['app.css'], // <= Load styling for the app.
+                libs: [] // <= Allready included libs, put not loaded per default. Example json2xml.js
+            };        
+    };    
+    
+    //initialize   
+    self.initialize = function (node, viewModel) {               
+        var data = self.config.fields;
+        data = JSON.stringify(data);                      
+        data  = JSON.parse(data);
+                
+        var valArray = [];
+        var colors = [];
+        jQuery.each(data, function (i, val){                    
+            valArray.push(window.external.ActiveInspector.Controls.GetText(val.field));
+            //alert(viewModel.person. +val.field + .text);
+            colors.push(val.color);           
+        });
+        go(valArray,colors);
+        return viewModel;
+    };
+
+    function go(valArray,colors) {    
+        var ctx = $("#myChart").get(0).getContext("2d");                   
+        var data = "["
+        for (var i = 0; i < valArray.length; i++) {                
+            if (valArray[i].indexOf("kr") >= 0) {                
+                var d = valArray[i].split(' ');                                                 
+                d = removeAllDots(d[0]);                
+                d = Math.round(d.replace(',','.'));                                
+                data = data + "{\"value\" : "+ d +", \"color\" : \"" + colors[i] +"\"}"
+                data = data + ",";        
+            }
+            else
+            {                               
+            var x = valArray[i].replace(/\s/g, '');                        
+            data = data + "{\"value\" : "+ valArray[i].replace(/\s/g, '') +", \"color\" : \"" + colors[i] +"\"}"
+            data = data + ",";        
+            }
+        }            
+        data = data.substring(0,data.length - 1) + "]";       
+        data = JSON.parse(data);
+       
+        var options = self.config.options;        
+        new Chart(ctx).Doughnut(data,options);
+    };    
+
+    function removeAllDots(str)
+    {
+        var index = str.indexOf('.');
+        if (index > -1){
+            str = str.substr( 0, index ) + 
+                str.slice( index ).replace( /\./g, '' );
+        }
+        return str;    
+    }
+
+});
