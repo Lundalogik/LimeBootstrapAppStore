414dbbfe2014aeb09fd88406409d53cd931c2743
Filip Persson <filip.persson@me.com>
2014-12-11 07:18:30 +0100

    Rewrite of data loading in checklist

--- checklist/app.js
+++ checklist/app.js
@@ -2,19 +2,24 @@ lbs.apploader.register('checklist', function () {
     var self = this;
     //config
     self.config = function(appConfig){
-        this.name = appConfig.name || 'Checklista'
-        this.xmlFieldName = appConfig.xmlFieldName || 'checklist'
-        this.canBeUnchecked = appConfig.canBeUnchecked || false
-        this.canAddTask = appConfig.canAddTask || false
-        this.allowRemove = appConfig.allowRemove || false
-        this.dataSources = [
-            {type: 'xml', source: 'Checklist.Initialize,' + this.xmlFieldName, alias:'checklistdata'}
-        ]
-        this.resources= {
+        this.name = appConfig.name || 'Checklista';
+        this.xmlFieldName = appConfig.xmlFieldName;
+        this.canBeUnchecked = appConfig.canBeUnchecked || false;
+        this.canAddTask = appConfig.canAddTask || false;
+        this.allowRemove = appConfig.allowRemove || false;
+        this.autoCreate = appConfig.autoCreate || true;
+        this.dataSources = new function(){
+            if(this.autoCreate){
+                return [{type: 'xml', source: 'Checklist.Initialize,' + this.xmlFieldName, alias:'checklistdata'}];
+            }else{
+                return [];
+            }
+        };
+        this.resources = {
             scripts: ['placeholders.min.js'],
             styles: ['checklist.css'],
             libs: ['json2xml.js']
-        }
+        };
     },
 
     //initialize
@@ -28,7 +33,17 @@ lbs.apploader.register('checklist', function () {
                 isChecked : ko.observable(false),
                 checkedDate : ko.observable(""),
                 checkedBy : ko.observable("")
-            }
+            };
+        }
+
+        function checklistItem(rawChecklistItem) {
+            this.idchecklist = "";
+            this.order = "";
+            this.title = "";
+            this.mouseover = "";
+            this.isChecked = ko.observable(false);
+            this.checkedDate = ko.observable("");
+            this.checkedBy = ko.observable("");
         }
 
         self.registerHandlers();
@@ -39,7 +54,7 @@ lbs.apploader.register('checklist', function () {
         function ChecklistModel(xmlchecklist) {
             var me = this;
 
-            me.tasks = ko.observableArray();
+            me.checklist = ko.observableArray();
             //populate tasks
             if(xmlchecklist){
                 if ($.isArray(xmlchecklist.checklist)){
@@ -86,19 +101,23 @@ lbs.apploader.register('checklist', function () {
                         task.checkedDate(moment().toISOString());
                         task.checkedBy(lbs.limeDataConnection.ActiveUser.Name);
                         if(task.idchecklist){
-                            task.isChecked (lbs.common.executeVba("Checklist.PerfromAction," + task.idchecklist));
+                            task.isChecked (lbs.common.executeVba("Checklist.PerfromAction," + task.isChecked() +"," + task.idchecklist + "," + task.title));
                         }else{
                             task.isChecked(true);
                         }
                     }else{
-
                         if(self.config.canBeUnchecked){
-                            task.isChecked(false);
+                            if(task.idchecklist){
+								task.isChecked (lbs.common.executeVba("Checklist.PerfromAction," + task.isChecked() +"," + task.idchecklist + "," + task.title));
+							}else{
+								task.isChecked(false);
+							}
                             task.checkedDate("");
                             task.checkedBy("");
                         }
                     }
                     me.save();
+                    lbs.common.executeVba("Checklist.SaveInspector");
                 }catch(e){
                     task.isChecked(false);  
                 }
@@ -127,7 +146,7 @@ lbs.apploader.register('checklist', function () {
             me.save = function(){
                         var tempJSON = JSON.stringify({checklist:ko.toJS(me.tasks)});
                         var tempXML = "<xmlchecklist>" + json2xml($.parseJSON(tempJSON),'') + "</xmlchecklist>";
-                        lbs.common.executeVba("Checklist.Save," + tempXML + "," + self.config.xmlFieldName) ;
+                        lbs.common.executeVba("Checklist.Save," + tempXML + "," + self.config.xmlFieldName);
             }
         }
 
@@ -140,7 +159,7 @@ lbs.apploader.register('checklist', function () {
                     "order": "1",
                     "title": "Beställ diarienummer",
                     "mouseover": "Kontakta diariet och beställ diarienummer med rubrik Avtal om tillträde järnvägsföretag",
-                    "isChecked":true
+                    
                   },
                   {
                     "idchecklist": "1101",
@@ -155,39 +174,13 @@ lbs.apploader.register('checklist', function () {
                     "mouseover": "Skicka avtal samt Excelfil för kontaktuppgifter till företagets kontaktperson"
                   }
                 ]
-            }
+            };
 
         /**
         Return view model
         */
             return new ChecklistModel(appData.checklistdata.xmlchecklist);
-
-
-        
-    },
-
-    self.registerHandlers = function(){
-        var ENTER_KEY = 13;
-    // a custom binding to handle the enter key (could go in a separate library)
-        ko.bindingHandlers.enterKey = {
-            init: function (element, valueAccessor, allBindingsAccessor, data) {
-                var wrappedHandler, newValueAccessor;
-                // wrap the handler with a check for the enter key
-                wrappedHandler = function (data, event) {
-                    if (event.keyCode === ENTER_KEY) {
-                        valueAccessor().call(this, data, event);
-                    }
-                };
-                // create a valueAccessor with the options that we would want to pass to the event binding
-                newValueAccessor = function () {
-                    return {
-                        keyup: wrappedHandler
-                    };
-                };
-                // call the real event binding's init function
-                ko.bindingHandlers.event.init(element, newValueAccessor, allBindingsAccessor, data);
-            }
-        };
+  
     }
 });
 
