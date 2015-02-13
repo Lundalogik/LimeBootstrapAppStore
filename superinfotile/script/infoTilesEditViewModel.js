d2072a89f4aa8e31e278b8878aafdffe9da0d29e
LarsCJensen <lars.c.jensen@gmail.com>
2015-02-13 19:10:30 +0100

    New stuff

--- superinfotile/script/infoTilesEditViewModel.js
+++ superinfotile/script/infoTilesEditViewModel.js
@@ -1,15 +1,21 @@
 var infoTilesEditViewModel = function () {
     var self = this;
+    self.table = "table";
+    self.field = "fält";
+    self.type = "typ";
+    self.fieldsenabledforedit = false;
+    self.typeenabledforedit = false;
     self.getTables = function(){
-        var tables = lbs.common.executeVba("superinfotile.LoadClasses")
+        var tables = lbs.common.executeVba("superinfotile.LoadClasses");
         self.tables = new ko.observableArray([]);
-        self.tables = ko.ToJson(tables.split);
+        self.tables = ko.toJSON(tables.split());
+        self.fieldsenabledforedit = true;
     }    
 
-    self.getFields = function (){
-        alert("getFields");
-        //var fieldsJSON = lbs.common.executeVba("superinfotile.SaveInfotiles", infoTilesJSON))
-        //self.fields = new ko.observableArray([]);
-        //self.fields=
-    }
+    self.getFields = function (){        
+        var fieldsJSON = lbs.common.executeVba("superinfotile.SaveInfotiles," + self.table);
+        self.fields = new ko.observableArray([]);
+        self.fields = ko.toJSON(fieldsJSON.split());
+        self.typeenabledforedit = true;
+    }   
 }
