f4bbdce4b20cbd979e58d9511cb891d24f0344ae
Adrian Schmidt <adrian@stealmycode.se>
2015-02-13 17:43:14 +0100

    Second commit. Yeay for great commit-messages...

--- newscarousel/README.md
+++ newscarousel/README.md
@@ -1,1 +1,37 @@
-#Template project
+News Carousel
+=========
+
+Your own info channel witthin LIME Pro.
+
+
+Info
+----
+
+This app shows a carousel of brief information for the LIME Users. The app could, for example, be used to:
+
+* Inform that LIME Pro will be upgraded on friday between 13-16.
+* Give LIME Pro tip and trick of the week.
+* Remind everyone of friday's after work.
+
+It's an easy and visual way to communicate LIME Pro related things within LIME Pro.
+
+
+Installation
+-----------
+
+1. Copy “newscarousel” folder to the “apps” folder. 
+1. Run the attached SQL-script in order to create the necessary table and fields. 
+1. Restart LIME Pro if open.
+1. Add the following HTML to the Index ActionPad and add configuration:
+
+```html
+
+<div data-app="{app:'Newscarousel', 
+	config:{
+}
+</div>
+```
+
+Setup
+---
+The app should not need any further setup. Go to the News tab in LIME and add your first news!
--- newscarousel/app.js
+++ newscarousel/app.js
@@ -1,7 +1,6 @@
 lbs.apploader.register('newscarousel', function () {
     var self = this;
 
-    //config
     this.config = {
         dataSources: [
 
@@ -11,10 +10,9 @@ lbs.apploader.register('newscarousel', function () {
             styles: ['app.css'],
             libs: ['json2xml.js']
         }
-    },
+    };
 
-    //initialize
-    this.initialize = function (node, viewModel) {
+    this.initialize = function(node, viewModel) {
 
         //Use this method to setup you app.
         //
@@ -22,24 +20,26 @@ lbs.apploader.register('newscarousel', function () {
         //You may make any modifications you please to it or replace is with a entirely new one before returning it.
         //The returned viewmodel will be used to build your app.
 
-        viewModel.myappname = 'Nyheter';
-        viewModel.myapptext = 'Blah blah blah';
-
+        viewModel.uniqueId = Math.random().toString(36).substring(10);
         viewModel.news = ko.observableArray([
             {
                 heading: 'Nyheter i LIME Pro',
                 text: 'Nu finns en fin liten nyhets-snurra i LIME Pros actionpad!'
             },
             {
-                heading: 'Nyhetstorka',
+                heading: 'Nyhetstorka 1',
                 text: 'Så värst mycket mer kan jag inte komma på att skriva tyvärr…'
             },
             {
-                heading: 'Nyhetstorka',
+                heading: 'Nyhetstorka 2',
+                text: 'Så värst mycket mer kan jag inte komma på att skriva tyvärr… Så värst mycket mer kan jag inte komma på att skriva tyvärr… Så värst mycket mer kan jag inte komma på att skriva tyvärr… Så värst mycket mer kan jag inte komma på att skriva tyvärr… Så värst mycket mer kan jag inte komma på att skriva tyvärr…'
+            },
+            {
+                heading: 'Nyhetstorka 3',
                 text: 'Så värst mycket mer kan jag inte komma på att skriva tyvärr…'
             }
         ]);
 
         return viewModel;
-    }
+    };
 });
