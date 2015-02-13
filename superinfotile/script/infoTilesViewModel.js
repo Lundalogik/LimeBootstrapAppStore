03268b2696f31a6428972689770429a0a11ceee8
LarsCJensen <lars.c.jensen@gmail.com>
2015-02-13 17:43:31 +0100

    New infoTilesViewModel

---
+++ superinfotile/script/infoTilesViewModel.js
@@ -0,0 +1,14 @@
+var infoTilesViewModel = function () {
+    var self = this;
+    var infoTiles = new ko.observableArray([]);
+    self.saveInfoTiles = function () {
+        ko.ToJson(infoTiles);
+    },
+    self.loadInfoTiles = function () {
+        infoTiles(lbs.common.executeVba("superinfotile.LoadInfoTiles"));
+    },
+    self.addInfoTile = function () {
+        infoTiles.push([]);
+    }
+
+}
