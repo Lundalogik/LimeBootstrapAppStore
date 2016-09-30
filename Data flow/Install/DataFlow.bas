2089dcc742efc59754355aac0cd1886507bb673b
Tommy Lindh <tommy.lindh@lundalogik.se>
2016-09-30 15:42:13 +0200

    Data Flow: * This app will replace the old history flow app. * It can be used as the old one but can also be fetch from which table and such as you want.

---
+++ Data flow/Install/DataFlow.bas
@@ -0,0 +1,113 @@
+Attribute VB_Name = "DataFlow"
+Option Explicit
+
+Public Function GetInitialData(ByVal sStructureXml As String) As String
+On Error GoTo ErrorHandler
+    Dim sXml As String
+    Dim sErrorMessage As String
+    Dim oFilter As New LDE.Filter
+    Dim oView As New LDE.View
+    Dim oRecords As New LDE.Records
+    Dim oClass As LDE.Class
+    Dim oRelationField As LDE.field
+    Dim sRelatedClass As String
+    Dim oRecord As LDE.Record
+    Dim oInspector As Lime.Inspector
+    
+    Dim lngPageSize As Long
+    Dim sTableName As String
+    Dim sTitleFieldName As String
+    Dim sTypeFieldName As String
+    Dim sDateFieldName As String
+    Dim sRelationFieldName As String
+    Dim sNoteFieldName As String
+    Dim sClickableRelationFieldName As String
+    
+    Dim oXml As New MSXML2.DOMDocument60
+    
+    If oXml.loadXML(sStructureXml) Then
+        lngPageSize = VBA.CLng(oXml.firstChild.selectSingleNode("pageSize").Text)
+        sTableName = oXml.firstChild.selectSingleNode("tableName").Text
+        sTitleFieldName = oXml.firstChild.selectSingleNode("titleFieldName").Text
+        sTypeFieldName = oXml.firstChild.selectSingleNode("typeFieldName").Text
+        sDateFieldName = oXml.firstChild.selectSingleNode("dateFieldName").Text
+        sRelationFieldName = oXml.firstChild.selectSingleNode("relationFieldName").Text
+        sNoteFieldName = oXml.firstChild.selectSingleNode("noteFieldName").Text
+        sClickableRelationFieldName = oXml.firstChild.selectSingleNode("clickableRelationFieldName").Text
+    End If
+    
+    Set oInspector = Application.ActiveInspector
+    
+    sXml = "<initialData>"
+    Set oClass = Database.Classes.Lookup(sTableName, lkLookupClassByName)
+    If Not oClass Is Nothing Then
+    
+        Set oRelationField = oClass.Fields.Lookup(sRelationFieldName, lkLookupFieldByName)
+        If Not oRelationField Is Nothing Then
+            sRelatedClass = oRelationField.LinkedField.Class.Name
+            If Globals.VerifyInspector(sRelatedClass, oInspector, True) Then
+                Call oView.Add(sTitleFieldName)
+                Call oView.Add(sTypeFieldName)
+                Call oView.Add(sDateFieldName, lkSortDescending)
+                Call oView.Add(sRelationFieldName)
+                Call oView.Add(sNoteFieldName)
+                Call oView.Add(sClickableRelationFieldName)
+                
+                Call oFilter.AddCondition(sRelationFieldName, lkOpEqual, oInspector.Record.ID)
+
+                Call oRecords.Open(oClass, oFilter, oView, lngPageSize)
+                sXml = sXml & "<dataFlows>"
+                For Each oRecord In oRecords
+                    sXml = sXml & "<dataFlow>"
+                    
+                    Dim sDate As String
+                    Dim sTime As String
+                    
+                    sDate = VBA.Format(oRecord.Value(sDateFieldName), "YYYY-MM-DD")
+                    sTime = VBA.Format(oRecord.Value(sDateFieldName), "HH:MM")
+                    
+                    Call AddXmlElement(sXml, "limeid", VBA.CStr(oRecord.ID))
+                    Call AddXmlElement(sXml, "title", VBA.CStr(oRecord.Text(sTitleFieldName)))
+                    Select Case oRecord.Fields(sTypeFieldName).Type
+                        Case lkFieldTypeOption
+                            Call AddXmlElement(sXml, "type", oRecord.GetOptionKey(sTypeFieldName))
+                        Case Else
+                            Call AddXmlElement(sXml, "type", oRecord.Text(sTypeFieldName))
+                    End Select
+                    
+                    Call AddXmlElement(sXml, "date", sDate)
+                    Call AddXmlElement(sXml, "time", sTime)
+                    Call AddXmlElement(sXml, "note", VBA.Left(oRecord.Text(sNoteFieldName), 200))
+                    
+                    If VBA.IsNull(oRecord.Value(sClickableRelationFieldName)) = False Then
+                        Call AddXmlElement(sXml, "clickableRelation_id", oRecord.Value(sClickableRelationFieldName))
+                        Call AddXmlElement(sXml, "clickableRelation_text", oRecord.Text(sClickableRelationFieldName))
+                    End If
+                    
+                    sXml = sXml & "</dataFlow>"
+                Next oRecord
+                sXml = sXml & "</dataFlows>"
+            End If
+        Else
+            sErrorMessage = Lime.FormatString("Can not find the relation field: '%1' on table '%2'", sRelationFieldName, sTableName)
+        End If
+    Else
+        sErrorMessage = Lime.FormatString("Can not find the table: '%1'", sTableName)
+    End If
+    
+    
+    sXml = sXml & "</initialData>"
+    GetInitialData = sXml
+Exit Function
+ErrorHandler:
+    Call UI.ShowError("DataFlow.GetInitialData")
+End Function
+
+
+Private Sub AddXmlElement(ByRef sXml As String, ByVal sElementName As String, ByVal sElementValue As String)
+On Error GoTo ErrorHandler
+    sXml = sXml & Lime.FormatString("<%1><![CDATA[%2]]></%1>", sElementName, sElementValue)
+Exit Sub
+ErrorHandler:
+    Call UI.ShowError("DataFlow.AddXmlElement")
+End Sub
---
+++ Data flow/app.js
@@ -0,0 +1,138 @@
+lbs.apploader.register('Data flow', function () {
+    var self = this;
+
+
+    /*Config (version 2.0)
+        This is the setup of your app. Specify which data and resources that should loaded to set the enviroment of your app.
+        App specific setup for your app to the config section here, i.e self.config.yourPropertiy:'foo'
+        The variabels specified in "config:{}", when you initalize your app are available in in the object "appConfig".
+    */
+    self.config =  function(appConfig) {
+        this.appConfig = {};
+
+        this.appConfig.tableStructure = this.appConfig.tableStructure || {};
+
+        this.appConfig.tableStructure.pageSize =  appConfig.tableStructure.pageSize || 5;
+        this.appConfig.tableStructure.tableName =  appConfig.tableStructure.tableName || 'history';
+        this.appConfig.tableStructure.titleFieldName =  appConfig.tableStructure.titleFieldName || 'type';
+        this.appConfig.tableStructure.typeFieldName =  appConfig.tableStructure.typeFieldName || 'type';
+        this.appConfig.tableStructure.dateFieldName =  appConfig.tableStructure.dateFieldName || 'date';
+        this.appConfig.tableStructure.relationFieldName =  appConfig.tableStructure.relationFieldName || 'company';
+        this.appConfig.tableStructure.noteFieldName =  appConfig.tableStructure.noteFieldName || 'note';
+        this.appConfig.tableStructure.clickableRelationFieldName =  appConfig.tableStructure.clickableRelationFieldName || 'person';
+
+        this.appConfig.defaultIcon = appConfig.defaultIcon || 'fa-comment-o';
+
+        var defaultTypes = {
+            'comment': 'fa-comment-o',
+            'customervisit': 'fa-group',
+            'noanswer': 'fa-thumbs-o-down',
+            'receivedemail': 'fa-envelope-o',
+            'salescall': 'fa-money',
+            'sentemail': 'fa-paper-plane-o',
+            'talkedto': 'fa-phone',
+        };
+
+        this.appConfig.typeMapping = appConfig.typeMapping || {};
+
+        for (var type in defaultTypes) {
+            if (!this.appConfig.typeMapping[type]) {
+                this.appConfig.typeMapping[type] = defaultTypes[type];
+            }
+        }
+
+
+        this.dataSources = [
+        ];
+
+        this.resources = {
+            scripts: ['VerticalTimeline/js/modernizr.custom.js'], // <= External libs for your apps. Must be a file
+            styles: ['app.css', 'VerticalTimeline/css/component.css', 'VerticalTimeline/css/default.css'], // <= Load styling for the app.
+            libs: [] // <= Allready included libs, put not loaded per default. Example json2xml.js
+        };
+    };
+
+    //initialize
+    /*Initialize
+        Initialize happens after the data and recources are loaded but before the view is rendered.
+        Here it is your job to implement the logic of your app, by attaching data and functions to 'viewModel' and then returning it
+        The data you requested along with localization are delivered in the variable viewModel.
+        You may make any modifications you please to it or replace is with a entirely new one before returning it.
+        The returned viewModel will be used to build your app.
+        
+        Node is a reference to the HTML-node where the app is being initalized form. Frankly we do not know when you'll ever need it,
+        but, well, here you have it.
+    */
+
+    self.initialize = function (node, viewModel) {
+        var appConfig = self.config.appConfig;
+        
+        viewModel.dataFlows = ko.observableArray();
+
+        var xmlParam = '<xmlStructure>';
+        xmlParam += '<pageSize>' + appConfig.tableStructure.pageSize.toString() + '</pageSize>';
+        xmlParam += '<tableName>' + appConfig.tableStructure.tableName.toString() + '</tableName>';
+        xmlParam += '<titleFieldName>' + appConfig.tableStructure.titleFieldName.toString() + '</titleFieldName>';
+        xmlParam += '<typeFieldName>' + appConfig.tableStructure.typeFieldName.toString() + '</typeFieldName>';
+        xmlParam += '<dateFieldName>' + appConfig.tableStructure.dateFieldName.toString() + '</dateFieldName>';
+        xmlParam += '<relationFieldName>' + appConfig.tableStructure.relationFieldName.toString() + '</relationFieldName>';
+        xmlParam += '<noteFieldName>' + appConfig.tableStructure.noteFieldName.toString() + '</noteFieldName>';
+        xmlParam += '<clickableRelationFieldName>' + appConfig.tableStructure.clickableRelationFieldName.toString() + '</clickableRelationFieldName>';
+        xmlParam += '</xmlStructure>';
+
+        var sourceString = 'DataFlow.GetInitialData, ' + xmlParam;
+
+        var initialData = lbs.loader.loadDataSources({}, [{
+            type: 'xml',
+            source: sourceString,
+            PassInspectorParam: false,
+            alias: 'initialData'
+        }], true);
+
+        initialData = (initialData && initialData.initialData && initialData.initialData.initialData) || {};
+        var dataFlows = (initialData && initialData.dataFlows && initialData.dataFlows.dataFlow) || null;
+        if(dataFlows) {
+            dataFlows = dataFlows.length && dataFlows || [dataFlows];
+            for (var i = 0; i < dataFlows.length; i++) {
+                var dataFlow = new DataFlow(dataFlows[i], appConfig);
+                viewModel.dataFlows.push(dataFlow);
+            };
+        }
+        
+        return viewModel;
+    };
+});
+
+function DataFlow(rawDataFlowObj, appConfig) {
+    this.title = rawDataFlowObj.title && rawDataFlowObj.title['#cdata'] || '';
+    this.type = rawDataFlowObj.type && rawDataFlowObj.type['#cdata'] || '';
+
+    this.clickableRelation = rawDataFlowObj.clickableRelation_id && {
+        id: rawDataFlowObj.clickableRelation_id['#cdata'],
+        text: rawDataFlowObj.clickableRelation_text['#cdata']
+    } || null;
+
+    this.text = rawDataFlowObj.note && rawDataFlowObj.note['#cdata'] || '';
+    this.date = rawDataFlowObj.date && rawDataFlowObj.date['#cdata'] || '';
+    this.time = rawDataFlowObj.time && rawDataFlowObj.time['#cdata'] || '';
+    this.limeid = rawDataFlowObj.limeid['#cdata'];
+    
+    this.icon = GetIconByType(this.type, appConfig);
+
+    this.openDataFlowPost = function() {
+        var limelink = lbs.common.createLimeLink(appConfig.tableStructure.tableName, this.limeid);
+        document.location.href(limelink);
+    };
+
+    this.openClickableRelationPost = function(self, event) {
+        event.stopPropagation();
+        var limelink = lbs.common.createLimeLink(appConfig.tableStructure.clickableRelationFieldName, this.clickableRelation.id);
+        document.location.href(limelink);
+    };
+
+    return this;
+};
+
+function GetIconByType(type, appConfig) {
+    return appConfig.typeMapping[type] || appConfig.defaultIcon;
+}
